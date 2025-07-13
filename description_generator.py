de71052d65ede1b209e5e355c629e30961b03b92
Rohit Anish <rohitanish158@gmail.com>
2025-07-13 12:59:42 +0530

    shaped code

--- description_generator.py
+++
@@ -1,2 +0,0 @@
-def generate_description(detected_objects):
-    return "You are near a " + " and a ".join(detected_objects) + "."
--- kaggle.py
+++
@@ -1,6 +0,0 @@
-import kagglehub
-
-# Download latest version
-path = kagglehub.model_download("tensorflow/ssd-mobilenet-v2/tensorFlow2/fpnlite-320x320")
-
-print("Path to model files:", path)
